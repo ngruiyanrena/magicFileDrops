58e69ec8a29e4054a8183e7a9cab127afef1b365
Rena Ng <e0772915@u.nus.edu>
2024-05-09 16:36:29 +0800

    prompt

--- magicFileProcessing.py
+++ magicFileProcessing.py
@@ -4,6 +4,7 @@ import openai
 from datetime import timezone 
 import datetime 
 import json
+import numpy as np 
 
 OPENAI_API_KEY = '' 
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
@@ -13,23 +14,24 @@ client = openai.OpenAI(
 )
 
 system_prompt = f"""
-    I require two extraction and fill formats: (i) individual lines (per employee) and (ii) summary totals (aggregate).
+    Extract and return payroll details for each indivdual employee listed in the data. 
     
-    Universally, payrolls follow a standard bridge for each period.  
-    ```
-    Gross Salary 
-    Add: Additions (e.g. bonuses, employee claims reimbursements, commissions)
-    Less: Deductions (e.g. employee statutory contributions of all types, voluntary contributions of all types, income taxes withheld)
-    Less: Clawbacks/Paybacks/NoPay Leave
-    = Take-home Salary
+    For each employee, retrieve the following six key values:
+        1. employee_name: The full name of employee. 
+        2. gross_salary: The basic pay or salary before any deductions. This is the total amount earned by the employee before tax or other deductions. 
+        3. additions: The total of all bonuses, reimbursements, or claims, including meal and transport allowances, added to the basic pay.
+        4. deductions: 
+            - The total of all deductions from the salary, including employee contributions such as CPF, CDAC, ECF, or other voluntary deductions. 
+            - DO NOT include SDL or employer CPF contribution. 
+        5. clawbacks: The total of all deductions for leave payment, including no pay leave, or any unpaid leaves taken by the employee.
+        6. take_home_salary: The net salary amount that the employee receives after all additions and deductions. Ensure the value is directly extracted from the data. Do not perform any calculations for this field.
 
-    Add: Deductions (from above)
-    Add: Employer Contributions
-    = Wages Payable ("total cost to company")
-    ```
-
-    For each individual line per employee, provide it in this specified json structure as follows:
-    Return only absolute values (i.e no '+' or '-'). 
+     Compliance checks:
+    - Verify that the following equation holds true for each record: take_home_salary = gross_salary + additions - deductions - clawbacks.
+    - If the equation does not balance, review all components for accuracy.
+    - Present all values in absolute terms (no '+' or '-'). Use '0' for any missing data.
+    
+    Present the data in the following structured JSON format:
     ```
     {{
         "employees": [
@@ -41,32 +43,12 @@ system_prompt = f"""
                 "clawbacks": "xxx",
                 "take_home_salary": "xxx"
             }}
-        ]
-    }}
-    ```
-
-    For each summary total, provide it in this specified json strucutre as follows:
-    Return only absolute values (i.e no '+' or '-'). 
-    ```
-    {{
-        "summaryTotals": [
-            {{
-                "deductions": "xxx",
-                "employer_contributions": "xxx",
-                "wages_payable": "xxx"
-            }}
-        ]
-    }}
-    ```
- 
-    Find month and year this payroll file is for. 
-    ```
-    {{
-        "date": "Jan 2023"
+        ],
+        "date": "xxx" // Find month and year this payroll file is for. eg. "Jan 2023"
     }}
     ```
     
-    Response should only follow the provided structures and not deviate from it. Return only a combined valid json. 
+    Response should only follow the provided structure and not deviate from it. Return a valid json. 
     """
 
 
@@ -74,21 +56,19 @@ system_prompt = f"""
 def call_gpt(df, prompt, user_prompt):
     try:
         full_prompt = f"""
-            {prompt}
-            
-            Additional User Instructions: {user_prompt}
-
-            Here is the file to create the journal entry. 
+            Here is the payroll file to analyze. 
             {df}
+            {prompt}
+            Additional User Instructions: {user_prompt}
         """
         response = client.chat.completions.create(
             model="gpt-4-turbo",
             messages=[
-                {"role": "system", "content": "You are a skilled accountant, with knowledge in all aspects of accounting. \
-                 You will be analyzing financial payroll files and creating journal entires for the company."},
+                {"role": "system", "content": "You are a skilled accountant with comprehensive knowledge in all aspects of payroll accounting. \
+                 You will be analyzing financial payroll files, ensuring accuracy and compliance with the stated requirements."},
                 {"role": "user", "content": full_prompt}
             ],
-            temperature=0.5,
+            temperature=0.2,
             max_tokens=600
         )
 
@@ -106,6 +86,13 @@ def convert_to_df_employees(employees):
     print("employees: ", employees)
     df = pd.DataFrame(employees)
 
+    columns = ["gross_salary", "additions", "deductions", "clawbacks", "take_home_salary"]
+    df[columns] = df[columns].apply(pd.to_numeric, errors='coerce')
+
+    total = df.select_dtypes(include=[np.number]).sum()
+    total['employee_name'] = "Total"
+    df = df.append(total, ignore_index=True)
+
     output_df = pd.DataFrame({
             "Name of Employee": df["employee_name"],
             "Gross Salary": df["gross_salary"],
@@ -117,17 +104,18 @@ def convert_to_df_employees(employees):
     
     return output_df
 
-def convert_to_df_summary_totals(summary_totals):
-    print("summary totals: ", summary_totals)
-    df = pd.DataFrame(summary_totals)
+# def convert_to_df_summary_totals(summary_totals):
+#     print("summary totals: ", summary_totals)
+#     df = pd.DataFrame(summary_totals)
 
-    output_df = pd.DataFrame({
-                "Add: Deductions": df["deductions"],
-                "Add: Employer Contributions": df["employer_contributions"],
-                "= Wages Payable": df["wages_payable"]
-            })
+#     output_df = pd.DataFrame({
+#                 "Add: Deductions": df["deductions"],
+#                 "Add: Employer Contributions": df["employer_contributions"],
+#                 "= Wages Payable": df["wages_payable"]
+#             })
     
-    return output_df
+#     return output_df
+
 
 def main():
     st.title("Smart Journal Entry Maker")
@@ -148,14 +136,15 @@ def main():
             utc_timestamp = utc_time.timestamp()
 
             df = pd.read_excel(uploaded_file, sheet_name=None) # some excel files have multiple sheets
+
             response = call_gpt(df, system_prompt, custom_prompt) 
             processed_data_employees = convert_to_df_employees(response['employees'])
-            processed_data_summary_totals = convert_to_df_summary_totals(response['summaryTotals'])
+            # processed_data_summary_totals = convert_to_df_summary_totals(response['summaryTotals'])
 
             st.write(f"Analysis for {uploaded_file.name}:")
             st.info(f"Payroll for {response['date']}")
             st.write(processed_data_employees)
-            st.write(processed_data_summary_totals)
+            # st.write(processed_data_summary_totals)
             st.write(f"utc timestamp: {utc_timestamp}")
 
 if __name__ == "__main__":
